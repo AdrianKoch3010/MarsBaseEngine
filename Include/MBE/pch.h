ad8b0f685bfb9ae56fa952ab533e22ad2da329b4
AdrianKoch3010 <adriankoch3010@gmail.com>
2019-03-11 21:29:51 +0100

    Added precompiled header file

---
+++ Include/MBE/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// General
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <thread>
+#include <math.h>
+#include <regex>
+
+// Data structures
+#include <string>
+#include <vector>
+#include <array>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <unordered_set>
---
+++ Source/MBE/pch.cpp
@@ -0,0 +1,3 @@
+
+
+#include "MBE/pch.h"
